7ceb26176a64918618506241596ac91798cb3037
Preston Chapman <pchapman82070@uat.edu>
2024-07-01 12:42:20 -0700

    Add Main Frame

---
+++ src/rpg/java/Frames/MainFrame.java
@@ -0,0 +1,11 @@
+package rpg.java.Frames;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        this.setSize(500,500);
+        this.setTitle("RPG | Preston C");
+        this.setVisible(true);
+    }
+}
--- src/rpg/java/Main.java
+++ src/rpg/java/Main.java
@@ -1,7 +1,9 @@
 package rpg.java;
 
+import rpg.java.Frames.MainFrame;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        MainFrame frame = new MainFrame();
     }
 }
