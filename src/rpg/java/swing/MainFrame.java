b8690674fe55f84116eec72d47f2e85889223e4e
Preston Chapman <pchapman82070@uat.edu>
2024-07-03 13:06:48 -0700

    Exit on close

--- src/rpg/java/swing/MainFrame.java
+++ src/rpg/java/swing/MainFrame.java
@@ -9,6 +9,7 @@ import rpg.java.swing.Panels.Panels.RegisterPanel;
 public class MainFrame extends JFrame{
     private final RegisterPanel REGISTER_PANEL = new RegisterPanel();
     public MainFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(Main.WIDTH, Main.HEIGHT);
         this.add(this.REGISTER_PANEL);
         this.setVisible(true);
